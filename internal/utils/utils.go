0007fd1425ac56d0794864cf337035f6da57b1c6
Yankee Maharjan <yankee.exe@gmail.com>
2022-05-01 11:47:38 +0545

    Convert duration to seconds

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,11 +1,8 @@
 package utils
 
 import (
-	"fmt"
-	"os"
-	"regexp"
-
 	"github.com/gookit/color"
+	"os"
 )
 
 func CheckIfError(err error) {
@@ -25,44 +22,15 @@ to create a new slack profile`)
 	os.Exit(1)
 }
 
-/*
-	Parses user duration for minute, hour, and day.
-	Defaults to minutes
-
-	Check the duration string has one of the suffix  (minutes, hour, days) in a array.
-	If it has value in that array then take that suffix and get numbers our of it.
-
-	@CONDITIONS
-	minutes cannot be more than 43800
-	hours cannot be more than 730
-	day cannot be more than 30
-
-	@REGEX:
-		(?P<Name>pattern) = match group
-		^\d+ = starts with any digits with one or more matches
-		.? = optional any character after the digits
-		---
-		-- Matches for duration suffix
-		(m$|min$|minutes?|h$|hr$|hours?|d$|days?)?
-		$ = match with character/word ending before the symbol
-		| = OR
-*/
-func ParseDuration(time string) {
-	r := regexp.MustCompile(`(?P<Period>^\d+).?(?P<Duration>m$|min$|minutes?|h$|hr$|hours?|d$|days?)?`)
-	match := r.FindStringSubmatch(time)
-
-	if len(match) == 0 {
-		color.Red.Println(`Invalid status duration
-Use 'st help add' for more information.
-		`)
-		os.Exit(1)
+// DurationConverter takes period and duration for a status and converts it into minutes
+func DurationConverter(period int, duration string) int {
+	switch duration {
+	case "m", "min", "mins", "minutes":
+		return period
+	case "h", "hr", "hour", "hours":
+		return period * 60
+	case "d", "day", "days":
+		return period * 1440
 	}
-
-	statusPeriod := match[1]
-	if match[2] == "" {
-		match[2] = "minutes"
-	}
-	fmt.Println("status period:", statusPeriod)
-	fmt.Println("status duration", match[2])
-
+	return 0
 }
